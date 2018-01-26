286e74838ec2d3333800bf8125ca3a23d776564f
Tim Huisman <timhuisman910@gmail.com>
2018-01-26 23:38:10 +0100

    Binary licenseplates now get rotated better

--- src/Copy_of_getPlateEdit.m
+++ src/Copy_of_getPlateEdit.m
@@ -2,7 +2,7 @@ clear all;
 run('C:\Program Files\DIPimage 2.9\dipstart.m');
 video = VideoReader('TrainingVideo.avi');
 %frame = imread('firstframe.png');
-frame = read(video, 50);
+frame = read(video, 10);
 
 % Removing objects that are not within a certain color range which represents a
 % licenseplate
@@ -34,43 +34,72 @@ measurements2 = regionprops(rotatedBinary, 'BoundingBox');
 % boundingbox
 rotatedOriginal = imrotate(frame, 1 - angle);
 croppedPlate = imcrop(rotatedOriginal, measurements2.BoundingBox);
-figure;
-image(croppedPlate);
-sharpened = imsharpen(croppedPlate);
 
-I = rgb2gray(sharpened);
+
+I = rgb2gray(croppedPlate);
 BW = imbinarize(I);
-
 
 Icorrected = imtophat(I, strel('disk', 50));
 
 BW1 = imbinarize(Icorrected);
 complement = imcomplement(BW1);
-size(complement, 2)
 finalPlate = bwareaopen(complement, 100, 8);
-
-figure;
-image(complement);
-colormap(gray(2));
-title('NOT OPENED');
+% finalPlate = flipdim(binary,1);
 
 figure;
 image(finalPlate);
+% colormap(gray(2));
+title('NOT ROTATED');
+hold on;
+L2 = bwlabel(finalPlate);
+s = regionprops(L2, 'BoundingBox');
+
+X1 = -1;
+Y1 = -1;
+X2 = -1;
+Y2 = -1;
+
+for i=1:numel(s)
+    Sdata=regionprops(L2 == i,'BoundingBox');
+    if abs(Sdata.BoundingBox(3)) < (1/2) * size(finalPlate, 2)
+       if X1 == -1
+           X1 = Sdata.BoundingBox(1);
+           Y1 = Sdata.BoundingBox(2);
+       end
+       if Sdata.BoundingBox(1) > X2
+           X2 = Sdata.BoundingBox(1) + Sdata.BoundingBox(3);
+           Y2 = Sdata.BoundingBox(2);
+       end
+    end
+end
+line([X1,X2],[Y1,Y2])
+hold off;
+
+angleDeg = radtodeg(tan(abs(Y2 - Y1)/abs(X2 - X1)))
+
+if isLarger(Y1, Y2)
+    rotated = imrotate(finalPlate, -angleDeg);
+else
+    rotated = imrotate(finalPlate, angleDeg);
+end
+
+I2 = im2uint8(finalPlate);
+I2(~finalPlate) = 200;
+I2(finalPlate) = 240;
+s2 = regionprops(L2, 'extrema');
+figure;
+imshow(I2, 'InitialMagnification', 'fit')
+hold on
+for k = 1:numel(s2)
+   e2 = s2(k).Extrema;
+   text(e2(1,1), e2(1,2), sprintf('%d', k));
+end
+hold off
+
+figure;
+image(rotated);
 colormap(gray(2));
-title('OPENED');
-
-
-
-% red2 = croppedPlate(:,:,1);
-% green2 = croppedPlate(:,:,2);
-% blue2 = croppedPlate(:,:,3);
-% 
-% thresh = (red2 + green2 + blue2) / 3;
-% 
-% HSV = rgb2hsv(croppedPlate);
-% value = HSV(:,:,3);
-% meanValue = mean(value);
-% locations = (value < meanValue & thresh < 92);
+title('ROTATED');
 
 
 
---
+++ src/isLarger.m
@@ -0,0 +1,7 @@
+function result = isLarger(Y1, Y2)
+    if Y1 > Y2
+        result = true;
+    else
+        result = false;
+    end
+end
